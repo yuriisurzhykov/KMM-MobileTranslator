f03984c72390459c73c81545a05baeb322e4bda7
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-02-16 03:08:21 -0800

    Changed using base64 for api token

--- androidApp/src/main/java/com/yuriisurzhykov/translator/android/core/data/ApiKeyFileProvider.kt
+++ androidApp/src/main/java/com/yuriisurzhykov/translator/android/core/data/ApiKeyFileProvider.kt
@@ -9,9 +9,7 @@ class ApiKeyFileProvider(private val context: Context) : ApiKeyProvider {
 
     override fun provideApiKey(): String {
         return context.assets.open("apikey.base64").bufferedReader().use { reader ->
-            reader.readLine().let { input ->
-                Base64.decode(input, Base64.DEFAULT).toString(StandardCharsets.UTF_8)
-            }
+            reader.readLine()
         }
     }
 }
